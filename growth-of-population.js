df82d0ffdd275008f4e97d308ea43fc10e2bcc68
Dmitro <dimka2701@gmail.com>
2021-01-10 19:56:55 +0200

    improve tests

--- growth-of-population.js
+++ growth-of-population.js
@@ -4,6 +4,16 @@ function nbYear(p0, percent, aug, p) {
     return 15;
 }
 
-console.log(nbYear(1500, 5, 100, 5000), 15);
-console.log(nbYear(1500000, 2.5, 10000, 2000000), 10);
-console.log(nbYear(1500000, 0.25, 1000, 2000000), 94);
+// testing
+
+const tests = [
+    [1000, 2, 50, 1200, 3],
+    [1500, 5, 100, 5000, 15],
+    [1500000, 2.5, 10000, 2000000, 10],
+    [1500000, 0.25, 1000, 2000000, 94]
+];
+
+tests.forEach(test => {
+    const result = nbYear(test[0], test[1], test[2], test[3]);
+    console.log(result, test[4], result === test[4]);
+});
