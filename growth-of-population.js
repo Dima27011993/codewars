402ef232dbc6e7046b8632ccc6037b793093ece7
Dmitro <dimka2701@gmail.com>
2021-01-10 19:37:04 +0200

    add tests

--- growth-of-population.js
+++ growth-of-population.js
@@ -1,2 +1,9 @@
 // https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/javascript
 
+function nbYear(p0, percent, aug, p) {
+    return 15;
+}
+
+console.log(nbYear(1500, 5, 100, 5000), 15);
+console.log(nbYear(1500000, 2.5, 10000, 2000000), 10);
+console.log(nbYear(1500000, 0.25, 1000, 2000000), 94);
