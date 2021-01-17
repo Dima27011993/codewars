dc7521f0f17641d0579d02fce63c72e95ed33542
Dmitro <dimka2701@gmail.com>
2021-01-17 21:44:06 +0200

    add first draft

---
+++ check-polindrom.js
@@ -0,0 +1,39 @@
+function solve(s) {
+  const chetnoe = isChetnoe(s);
+  console.log(chetnoe);
+
+  // if ne chetnoe remove middle symbol to make chetnoe
+
+  // compare first and last symbols (combinations)
+
+  return false;
+}
+
+// return true - dlina stroki chetnoe chisle
+// return false - ne chetnoe
+function isChetnoe(anything) {
+  const ostOtDeleniyaNaDva = anything.length % 2;
+  const chetnoe = ostOtDeleniyaNaDva === 0;
+  if (chetnoe) {
+    // console.log(true);
+    return false;
+  } else {
+    // console.log(false);
+    return true;
+  }
+  //   return anything.length % 2 === 0;
+}
+
+const tests = [
+  ["abba", true],
+  ["abaazaba", false],
+  //   [
+  //     "kxbkwgyydkcbtjcosgikfdyhuuprubpwthgflucpyylbofvqxkkvqthmdnywpaunfihvupbwpruwfybdmgeuocltdaidyyewmbzm",
+  //     true,
+  //   ],
+];
+
+tests.forEach((test) => {
+  const result = solve(test[0]);
+  console.log(result, test[1], result === test[1]);
+});
